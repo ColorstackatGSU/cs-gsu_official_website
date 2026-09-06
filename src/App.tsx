21cb6dc75dbeaee813e4524b2725da94ae07aa19
Uyiosa Nehikhuere <unehikhuere@gmail.com>
2026-09-06 12:05:01 -0400

    Remove Spiels page and associated assets

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import About from './pages/About';
 import Involvement from './pages/Involvement';
 import Students from './pages/Students';
 import Sponsors from './pages/Sponsors';
-import Spiels from './pages/Spiels';
 import ApplyLanding from './pages/Apply';
 import ApplyMember from './pages/Apply/Member';
 import ApplyEBoard from './pages/Apply/EBoard';
@@ -27,9 +26,6 @@ export default function App() {
           <Route path="involvement" element={<Involvement />} />
           <Route path="students" element={<Students />} />
           <Route path="sponsors" element={<Sponsors />} />
-          <Route path="spiels" element={<Spiels />} />
-          {/* singular link works too */}
-          <Route path="spiel" element={<Spiels />} />
           <Route path="apply" element={<ApplyLanding />} />
           <Route path="apply/member" element={<ApplyMember />} />
           <Route path="apply/e-board" element={<ApplyEBoard />} />
