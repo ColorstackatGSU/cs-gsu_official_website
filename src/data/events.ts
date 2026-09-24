3bbc88ca80aaa24c516bda5823af8c87b6692b33
VincePaul723 <vincepaul723@gmail.com>
2026-09-23 22:26:50 -0400

    Added the new batch of events to the events page - waiting on cs x nvidia banner (there's currentlly a place holder for it) and Tech League PIN link.

--- src/data/events.ts
+++ src/data/events.ts
@@ -30,7 +30,7 @@ export const eventYears: EventYear[] = [
           'Kick off the new academic year with ColorStack at GSU! Meet the new executive board, learn about our upcoming workshops, panels, and networking events, and connect with fellow tech students.',
         image: '/images/events/kickoff-fall-2026.png',
         tag: 'Social',
-        isUpcoming: true,
+        isUpcoming: false,
         link: 'https://pin.gsu.edu/event/12618217',
       },
       {
@@ -41,9 +41,53 @@ export const eventYears: EventYear[] = [
           'A collaborative AI Idea-a-Thon presented by SHPE and ColorStack at GSU! Brainstorm innovative AI solutions, collaborate with peers across disciplines, and pitch your ideas.',
         image: '/images/events/ai-ideathon.png',
         tag: 'Social',
-        isUpcoming: true,
+        isUpcoming: false,
         link: 'https://pin.gsu.edu/event/12716936',
       },
+      {
+        title: 'GSU Tech Clubs - Resume Workshop',
+        date: 'Sep 22, 2026',
+        semester: 'Fall 2026',
+        description:
+          'Get the ins and outs of perfecting your resume and standing out to recruiters.',
+        image: '/images/events/resume_workshop_1_2026.png',
+        tag: 'Social',
+        isUpcoming: false,
+        link: '',
+      },
+      {
+        title: 'Equifax X Colorstack & CS Club',
+        date: 'Sep 28, 2026',
+        semester: 'Fall 2026',
+        description:
+          'An event built to introduce students to Equifax, Technical Career Paths, and 2027 Internship and Rotational Opportunities.',
+        image: '/images/events/equifax_colorstack.png',
+        tag: 'Social',
+        isUpcoming: true,
+        link: 'https://pin.gsu.edu/event/12793088',
+      },
+      {
+        title: 'Progsu x ColorStack Tech League',
+        date: 'Sep 30, 2026',
+        semester: 'Fall 2026',
+        description:
+          'A semester-long, team-based competition. Teams of 3-4 tackle 5 milestones, rack up points pn the leaderboard, and finish with a Capstone Hackathon',
+        image: '/images/events/tech_league_3.jpg',
+        tag: 'Social',
+        isUpcoming: true,
+        link: '', 
+      },
+      {
+        title: 'ColorStack X NVIDIA',
+        date: 'Oct 8, 2026',
+        semester: 'Fall 2026',
+        description:
+          'Explore the future of technology and hear from an NVIDIA engineering leader about the innovation shaping our industry. Learn about opportunities and resume best practices.',
+        image: '/images/events/colorstackxnvidia_placeholder.png',
+        tag: 'Social',
+        isUpcoming: true,
+        link: 'https://pin.gsu.edu/event/12793269', 
+      }
     ],
   },
   {
